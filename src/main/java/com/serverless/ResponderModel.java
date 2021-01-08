4529b0be7d56c49682bb1b9f2f6c69ee41669d7c
Daniel Henao <dhenaos@devco.com.co>
2021-01-08 10:46:25 -0500

    Raiz cubica y cuadrada

--- src/main/java/com/serverless/ResponderModel.java
+++ src/main/java/com/serverless/ResponderModel.java
@@ -7,9 +7,6 @@ public class ResponderModel {
     private final String teamName = "D-ops";
 
     public String answer(String question) {
-        if ("".equals(question)){
-            return teamName;
-        }
 
         Matcher nameMatcher = Pattern.compile(".*what is your name").matcher(question);
         if (nameMatcher.matches()) {
@@ -26,9 +23,8 @@ public class ResponderModel {
             return String.valueOf(Integer.parseInt(id_44bdc450.group(1)) + Integer.parseInt(id_44bdc450.group(2)));
         }
 
-        Matcher largestMatcher = Pattern.compile(".*which of the following numbers is the largest: (\\d+)").matcher(question);
+        Matcher largestMatcher = Pattern.compile(".*which of the following numbers is the largest: (.*)").matcher(question);
         if (largestMatcher.matches()) {
-
             String[] numbers = largestMatcher.group(1).trim().split(", ");
             int largest = Integer.parseInt(numbers[0]);
             for (String number : numbers) {
@@ -39,6 +35,25 @@ public class ResponderModel {
 
             return String.valueOf(largest);
         }
+
+        Matcher id_4842a480 = Pattern.compile(".*what is (\\d+) multiplied by (\\d+)").matcher(question);
+        if (id_4842a480.matches()) {
+            return String.valueOf(Integer.parseInt(id_44bdc450.group(1)) * Integer.parseInt(id_44bdc450.group(2)));
+        }
+
+
+
+        Matcher id_77fb4110 = Pattern.compile(".*which of the following numbers is both a square and a cube: (\\d+), (\\d+)").matcher(question);
+        if (id_77fb4110.matches()) {
+            if ((Math.sqrt(Integer.parseInt(id_77fb4110.group(1))) % 1 == 0)&&(Math.cbrt(Integer.parseInt(id_77fb4110.group(1))) % 1 == 0)){
+                return id_77fb4110.group(1);
+            }else if ((Math.sqrt(Integer.parseInt(id_77fb4110.group(2))) % 1 == 0)&&(Math.cbrt(Integer.parseInt(id_77fb4110.group(2))) % 1 == 0)){
+                return id_77fb4110.group(2);
+            }else{
+                return "No da";
+            }
+        }
+
         return teamName;
     }
 
