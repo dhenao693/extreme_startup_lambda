2a212675c11458fd454215e9cfb19dde54c38037
Daniel Henao <dhenaos@devco.com.co>
2021-01-08 10:14:02 -0500

    Ajustes a primera ronda

--- src/main/java/com/serverless/ResponderModel.java
+++ src/main/java/com/serverless/ResponderModel.java
@@ -11,11 +11,32 @@ public class ResponderModel {
             return teamName;
         }
 
+        Matcher nameMatcher = Pattern.compile(".*what is your name").matcher(question);
+        if (nameMatcher.matches()) {
+            return teamName;
+        }
+
         Matcher sumMatcher = Pattern.compile(".*what is the sum of (\\d+) and (\\d+)").matcher(question);
         if (sumMatcher.matches()) {
             return String.valueOf(Integer.parseInt(sumMatcher.group(1)) + Integer.parseInt(sumMatcher.group(2)));
         }
-        
+
+        Matcher id_44bdc450 = Pattern.compile(".*what is (\\d+) plus (\\d+)").matcher(question);
+        if (id_44bdc450.matches()) {
+            return String.valueOf(Integer.parseInt(id_44bdc450.group(1)) + Integer.parseInt(id_44bdc450.group(2)));
+        }
+
+        Matcher largestMatcher = Pattern.compile(".*which of the following numbers is the largest: (\\d+)").matcher(question);
+        if (largestMatcher.matches()) {
+            int largest = 0;
+            String[] numbers = sumMatcher.group(1).split(", ");
+            for (String number : numbers) {
+                if (Integer.parseInt(number) > largest) {
+                    largest = Integer.parseInt(number);
+                }
+            }
+            return String.valueOf(largest);
+        }
         return teamName;
     }
 
